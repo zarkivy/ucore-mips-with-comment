55d720331a9ade146089a63f0ec193f893b776b9
IZAYOIGitHub <1906826229@qq.com>
2020-04-18 16:53:11 +0800

    update

--- kern/mm/vmm.h
+++ kern/mm/vmm.h
@@ -10,12 +10,14 @@
 #include <proc.h>
 #include <sem.h>
 
-//pre define
+// pre define
 struct mm_struct;
 
 // the virtual continuous memory area(vma)
+// 管理虚拟内存区域的数据结构
+// 一个 vma_struct 维护一段虚拟内存的信息：其开始与结束地址、rwx 权限
 struct vma_struct {
-    struct mm_struct *vm_mm; // the set of vma using the same PDT 
+    struct mm_struct *vm_mm; // the set of vma using the same PDT 指向管理自己的 mm_struct
     uintptr_t vm_start;      //	start addr of vma	
     uintptr_t vm_end;        // end addr of vma
     uint32_t vm_flags;       // flags of vma
@@ -25,6 +27,7 @@ struct vma_struct {
 #define le2vma(le, member)                  \
     to_struct((le), struct vma_struct, member)
 
+// vma 的 rwx 权限
 #define VM_READ                 0x00000001
 #define VM_WRITE                0x00000002
 #define VM_EXEC                 0x00000004
@@ -32,10 +35,12 @@ struct vma_struct {
 
 
 // the control struct for a set of vma using the same PDT
+// 一个进程的 4GB 虚拟内存空间使用一个 mm_struct 管理
+// mm_struct 管理该进程的所有 vma_struct
 struct mm_struct {
     list_entry_t mmap_list;        // linear list link which sorted by start addr of vma
     struct vma_struct *mmap_cache; // current accessed vma, used for speed purpose
-    pde_t *pgdir;                  // the PDT of these vma
+    pde_t *pgdir;                  // the PDT of these vma 该进程的页表地址
     int map_count;                 // the count of these vma
 	void *sm_priv;				   // the private data for swap manager
 	atomic_t mm_count;
